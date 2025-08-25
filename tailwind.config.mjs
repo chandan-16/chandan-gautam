302a1106f30ab7bc7d14eeed22ad45751c1885f3
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-25 16:40:26 +0530

    Making my first Nextjs Portfolio Website, done will all the file structure, assets and required tailwind CSS

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a'
+      }
+    },
+  },
+  plugins: [],
+}
+
