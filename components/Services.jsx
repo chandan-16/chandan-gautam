e323293d4771b60e81115d0f6cd2634bd22709fd
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-27 23:10:13 +0530

    Wrote the self defined css classes in the tailwind config mjs file to use in required component and started building the work component

--- components/Services.jsx
+++ components/Services.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Services = () => {
   return (
-    <div id="#services" className="w-full px-[12%] py-10 scroll-mt-20 mx-auto">
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20 mx-auto">
       <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
       <h2 className="text-center text-5xl font-Ovo">My Services</h2>
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
@@ -13,7 +13,7 @@ const Services = () => {
         visually appealing interfaces.
       </p>
 
-      <div className="grid grid-cols-4 gap-6 my-10">
+      <div className="grid grid-cols-auto gap-6 my-10">
         {
             serviceData.map(({ icon, title, description, link }, index) => (
                 <div key={index} className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1">
