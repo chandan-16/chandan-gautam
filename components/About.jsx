e95c09db0351cf8358d4265e70a16658e3448de9
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-27 22:53:04 +0530

    Updated the navbar my making it fixed scroll option & working on the Service page of the website

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from "@/assets/assets";
+import { assets, infoList, toolsData } from "@/assets/assets";
 import Image from "next/image";
 
 const About = () => {
@@ -33,13 +33,24 @@ const About = () => {
             {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li
                 key={index}
-                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff]"
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-black"
               >
                 <Image src={icon} alt={title} className="w-7" />
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                 <p className="text-gray-600 text-sm">{description}</p>
               </li>
             ))}
+          </ul>
+
+          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {
+              toolsData.map((tool, index) => (
+                <li key={index} className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500">
+                  <Image src={tool} alt="tool" className="w-5 sm:w-7" />
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
