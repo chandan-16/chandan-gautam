98c26cc6c99131d114e18f3d500cd3405c93115a
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-28 13:53:57 +0530

    Working on the footer component of the website and adding the dark and light mode to the website by using react hooks

---
+++ components/Footer.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import { assets } from './../assets/assets';
+
+const Footer = () => {
+  return (
+    <div className="mt-20">
+        <div className="text-center">
+            <h1 className="w-36 mx-auto mb-2 text-gray-700 font-semibold  text-3xl text-left align-middle">Gautam <span className="text-red-600 font-bold text-4xl -pr-[5px]">.</span></h1>
+            <div className="w-max flex items-center gap-2 mx-auto">
+                <Image src={assets.mail_icon} alt='footer-mail' className="w-6" />
+                chandangautam420.cg@gmail.com
+            </div>
+        </div>
+        <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[12%] mt-12 py-6">
+            <p>&copy; 2025 Chandan Gautam. All Rights Reserved.</p>
+            <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-">
+                <li><a target="_blank" href="https://github.com/chandan-16">Github</a></li>
+                <li><a target="_blank" href="https://www.linkedin.com/in/chandan1699/">LinkedIn</a></li>
+                {/* <li><a target="_blank" href="">Instagram</a></li> */}
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 import { useEffect, useRef, useState } from "react";
 
-const Navbar = () => {
+const Navbar = ({ isDarkMode, setIsDarkMode }) => {
 
     const [isScroll, setIsScroll] = useState(false);
     const sideMenuRef = useRef();
@@ -28,14 +28,14 @@ const Navbar = () => {
 
   return (
     <>
-    <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+    <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
     </div>
 
-    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`} >
+    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20 " : ""}`} >
         <a href="#top">
-            {/* <Image src={assets.logo} alt="" className="w-28 cursor-pointer mr-14" /> */}
-            <h1 className="text-gray-700 font-semibold text-3xl text-left align-middle">Gautam <span className="text-red-600 font-bold text-4xl -pr-[5px]">.</span></h1>
+            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt="" className="w-28 cursor-pointer mr-14" />
+            {/* <h1 className="text-gray-700 font-semibold text-3xl text-left align-middle">Gautam <span className="text-red-600 font-bold text-4xl -pr-[5px]">.</span></h1> */}
         </a>
 
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opaciy-50" }`}>
@@ -46,8 +46,9 @@ const Navbar = () => {
             <li><a href="#contact" className="font-Ovo">Contact me</a></li>
         </ul>
         <div className="flex items-center gap-4">
-            <button>
-                <Image src={assets.moon_icon} alt='' className="w-6" />
+            <button onClick={() => setIsDarkMode(previous => !previous)}>
+
+                <Image src={ isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className="w-6" />
             </button>
 
             <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo">Contact <Image src={assets.arrow_icon} alt="" className="w-3" /></a>
